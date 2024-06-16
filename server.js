6d693b16a376c3450084e45e49818ebc2b7ce9a2
saiteja-in <vurukondasaiteja13@gmail.com>
2024-06-16 23:14:36 +0530

    exercise2

--- server.js
+++ server.js
@@ -5,17 +5,22 @@ const server=http.createServer(app);
 const {Server}=require('socket.io')
 const io=new Server(server)
 const path=require('path');
-//we are wrapping http server inside socker io server
+//we are wrapping http server inside socket io server
 const PORT=3000;
+
+let users=[]
 
 app.get('/',(req,res)=>{
     res.sendFile(path.join(__dirname,'index.html'));
 })
 
 io.on('connection',(socket)=>{
-    socket.on("good",(msg)=>{
-        console.log(msg)
+    console.log(`a user is connected with id ${socket.id}`)
+    socket.on("send user info",userInfo=>{
+        users.push(userInfo)
+        console.log(userInfo)
     })
+    console.log(users)
 })
 
 
