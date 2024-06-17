67d2fd81dce1916e4082bb52a00c4a9374fcecde
saiteja-in <vurukondasaiteja13@gmail.com>
2024-06-17 13:19:34 +0530

    mood tracker(everyone except the sender) socket.emit

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.get('/',(req,res)=>{
 
 io.on('connection',(socket)=>{
         socket.on('send mood',(name,mood)=>{
-            io.emit('rec mood',name,mood)
+            socket.emit('rec mood',name,mood)
         })
 })
 
